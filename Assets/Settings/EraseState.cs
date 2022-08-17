115796752dafaae3c323210cbbfc4397cb59f23d
Vineeth <mamidipallyvineeth@gmail.com>
2022-08-17 16:44:59 +0530

    Adding Basic Game Functionality now Implimented working glitter function

---
+++ Assets/Settings/EraseState.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace StateMachine
+{
+	public class EraseState : MonoBehaviour
+	{
+		public EraseTarget eraseMechanic;
+
+		public float raycastDistance = 50f;
+
+		private Camera _camera;
+		private void Start()
+		{
+			_camera = Camera.main;
+		}
+		
+		private void Update()
+		{
+			Execute();
+		}
+
+		public void Execute()
+		{
+			if(Input.GetMouseButtonUp(0))
+			{
+				return;
+			}
+
+			if (!Input.GetMouseButton(0)) return;
+			
+			var ray = _camera.ScreenPointToRay(Input.mousePosition);
+			if (!Physics.Raycast(ray.origin, ray.direction, out var hit, raycastDistance)) return;
+			if (!hit.transform.root.CompareTag("EraseTarget")) return;
+
+			eraseMechanic.EraseAt(hit.point);
+
+		}
+	}
+}
