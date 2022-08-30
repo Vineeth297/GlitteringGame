255fad34d4e7d950f70bdbb03608ea6878d4ff4b
Vineeth <mamidipallyvineeth@gmail.com>
2022-08-30 14:14:30 +0530

    Added Reload button, level font, and background envi

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	private Camera _camera;
+
+	[SerializeField] private Transform drawing;
+	[SerializeField] private Transform camInitialTransform;
+	[SerializeField] private Transform camFinalTransform;
+	private void Start()
+	{
+		_camera = Camera.main;
+		//_camera.transform.position = camInitialTransform.position;
+		_camera.transform.DOMove(camFinalTransform.position, 3f);
+		_camera.transform.DORotate(Vector3.right * 90f, 3.25f).SetEase(Ease.Linear);
+		//		DOVirtual.DelayedCall(1.5f, ()=> 
+		//_camera.transform.DOLookAt(drawing.position, 3f);
+//		_camera.transform.DORotate(Vector3.right * 90f, 5f);
+	}
+	
+	
+}
